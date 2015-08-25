c2a4ca5c9e0ea4e794856e3e60a3943e307a98ae
Usman Tahir <ch.usman.tahir@gmail.com>
2015-08-25 13:53:15 +0500

    remote db

--- db.js
+++ db.js
@@ -4,20 +4,30 @@ var Server = mongo.Server,
     Db = mongo.Db,
     BSON = mongo.BSONPure;
 
-var server = new Server('localhost', 27017, {auto_reconnect: true});
-db = new Db('mba-deadlines', server);
+//var server = new Server('evennode.com', 27017, {auto_reconnect: true});
+//db = new Db('app_1092', server);
+var dbInstance = '';
 
-db.open(function(err, db) {
-    if(!err) {
-        console.log("Connected to 'mba-deadlines' database");
-        db.collection('schools', {strict:true}, function(err, collection) {
-            if (err) {
-                console.log("The 'schools' collection doesn't exist.");
-            }
-        });
-    }
+//db.open(function(err, db) {
+//    if(!err) {
+//        console.log("Connected to 'mba-deadlines' database");
+//        db.collection('schools', {strict:true}, function(err, collection) {
+//            if (collection) {
+//                console.log(collection.toString());
+//            }
+//            if (err) {
+//                console.log(err.toString());
+//            }
+//        });
+//    }
+//});
+
+// Connect using the connection string
+mongo.connect("mongodb://app_1092:app_1092@evennode.com:27017/app_1092", {native_parser:true}, function(err, db) {
+    if (err) console.log("err " + err.toString());
+    if (db) console.log("db " + db.toString());
+    module.exports.instance = db;
 });
 
-module.exports.server = server;
-module.exports.instance = db;
+// module.exports.server = server;
 module.exports.BSON = BSON;
