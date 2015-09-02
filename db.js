8ba7cbe53702b0a383eb02b10c07ceb6a93703c9
Usman Tahir <ch.usman.tahir@gmail.com>
2015-09-03 00:51:46 +0500

    heroku commit

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 var mongo = require('mongodb');
 
 // Connect using the connection string
-mongo.connect("mongodb://app_1092:app_1092@evennode.com:27017/app_1092", {native_parser:true}, function(err, db) {
+mongo.connect("mongodb://mba-admin:mba-admin@ds035563.mongolab.com:35563/mbadeadlines", {native_parser:true}, function(err, db) {
     if (err) console.log("err " + err.toString());
     if (db) console.log("db " + db.toString());
     module.exports.instance = db;
