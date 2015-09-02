a903958ddd55a5ea6d262af6121f94b2d9aae0c4
Usman Tahir <ch.usman.tahir@gmail.com>
2015-09-03 01:21:48 +0500

    cors added to top

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ var express = require('express'),
 
 var app = express();
 
+app.use(cors());
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
 app.use('/request', multipartMiddleware);
-app.use(cors());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
